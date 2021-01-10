0afee950908b005c1aec0fc31cab4980507adea8
Ricardosoz <ricardosoz7@protonmail.com>
2021-01-09 23:41:11 -0600

    Finished class 16

--- Notes.app/app.js
+++ Notes.app/app.js
@@ -1,10 +1,42 @@
 const validator = require('validator')
 const chalk = require('chalk')
+const yargs = require('yargs')
 const add = require('./utils')
 
+yargs.version('1.1.0')
 
-const command = process.argv[2]
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function(){
+        console.log('Adding a new note!')
+    }
+})
 
-if(command === 'hola'){
-    console.log(chalk.green.bold("Hola"))
-}
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function(){
+        console.log('Removing a note!')
+    }
+})
+
+// Create List command
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler: function(){
+        console.log('Listing all notes')
+    }
+})
+
+// Read List command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function(){
+        console.log('Reading a note')
+    }
+})
