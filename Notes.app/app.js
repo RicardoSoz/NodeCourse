aed87f2402ecaa75423251431fc5f34c482f6502
Ricardosoz <ricardosoz7@protonmail.com>
2021-01-09 22:51:40 -0600

    finished class 11

--- Notes.app/app.js
+++ Notes.app/app.js
@@ -1,4 +1,6 @@
+const validator = require('validator')
 const add = require('./utils')
+
 
 const name = 'Ricardo'
 
@@ -7,3 +9,7 @@ const sum = add.add (2, 2)
 console.log(name)
 console.log(add.name2)
 console.log(sum)
+
+console.log(validator.isEmail('Ricardo@mail.com'))
+
+console.log(validator.isURL('Ricardo@mail.com'))
