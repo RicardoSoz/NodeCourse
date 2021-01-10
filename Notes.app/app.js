c6a6292339a321b054a6f7acbdd6e63dac569f8d
Ricardosoz <ricardosoz7@protonmail.com>
2021-01-09 19:42:17 -0600

    Finished class 9

--- Notes.app/app.js
+++ Notes.app/app.js
@@ -3,5 +3,6 @@ const fs = require('fs');
 
 fs.writeFileSync('notes.txt', 'This file was created by Node.js!')
 
+ //Challenged: Append a message to notes.txt
 
-sajdlkdsajk
+ fs.appendFileSync('notes.txt', ' Appended text for the challenge')
